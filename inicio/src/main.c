dc936ca30985a48c0d46f274689dbc2feda46eb2
Daniel Scheuermann <scheuermann.daniel@edu.pucrs.br>
2026-03-10 14:14:10 +0000

    troco

---
+++ inicio/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int valor;
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &valor);
+    printf("Valor: %s!\n", valor);
+    int notas = 0;
+    printf("Quantidade de notas de R$100: ")
+    return 0;
+}
